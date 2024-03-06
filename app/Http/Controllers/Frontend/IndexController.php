780832c0bcb2c6f1ff6e8cc0072766796889d694
unknown <Emon>
2024-03-06 16:12:30 +0600

    product details part 3

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -15,6 +15,8 @@ class IndexController extends Controller
         $multi_imgs = MultiImg::where('product_id',$id)->get();
         $product_color = explode(',',$product->product_color);
         $product_size = explode(',',$product->product_size);
-        return view('frontend.product.product_details',compact('product','product_color','product_size','multi_imgs'));
+        $cat_id = $product->category_id;
+        $relatedProduct = Product::where('category_id',$cat_id)->where('id','!=',$id)->orderBy('id','desc')->limit(4)->get();
+        return view('frontend.product.product_details',compact('product','product_color','product_size','multi_imgs','relatedProduct'));
     }
 }
