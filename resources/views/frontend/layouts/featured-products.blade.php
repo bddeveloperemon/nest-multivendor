069950940f635daaaf56bc44dd1c0f8dde1f1e0f
unknown <Emon>
2024-03-21 11:03:47 +0600

    update wishlist,compare and quickview in some page and update details urls

--- resources/views/frontend/layouts/featured-products.blade.php
+++ resources/views/frontend/layouts/featured-products.blade.php
@@ -40,10 +40,11 @@
                                                     data-bs-toggle="modal" data-bs-target="#quickViewModal"
                                                     id="{{ $product->id }}" onclick="productView(this.id)"> <i
                                                         class="fi-rs-eye"></i></a>
-                                                <a aria-label="Add To Wishlist" class="action-btn small hover-up"
-                                                    href="shop-wishlist.html"><i class="fi-rs-heart"></i></a>
-                                                <a aria-label="Compare" class="action-btn small hover-up"
-                                                    href="shop-compare.html"><i class="fi-rs-shuffle"></i></a>
+                                                <a aria-label="Add To Wishlist" class="action-btn"
+                                                    id="{{ $product->id }}" onclick="addToWishlist(this.id)"><i
+                                                        class="fi-rs-heart"></i></a>
+                                                <a aria-label="Compare" class="action-btn" id="{{ $product->id }}"
+                                                    onclick="addToCompare(this.id)"><i class="fi-rs-shuffle"></i></a>
                                             </div>
                                             @php
                                                 $amount = $product->selling_price - $product->discount_price;
