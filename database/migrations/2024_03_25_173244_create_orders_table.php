ed51e0ca154eed7b45d8cdf39ebc00153123c7a3
unknown <Emon>
2024-03-26 00:11:36 +0600

    stripe gateway part 2 create model migration order and orderitem

---
+++ database/migrations/2024_03_25_173244_create_orders_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('division_id')->constrained('ship_divisions')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('district_id')->constrained('ship_districts')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('state_id')->constrained('ship_states')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('name');
+            $table->string('email');
+            $table->string('address')->nullable();
+            $table->string('phone');
+            $table->string('post_code')->nullable();
+            $table->text('notes')->nullable();
+            $table->string('payment_type')->nullable();
+            $table->string('payment_method')->nullable();
+            $table->string('transaction_id')->nullable();
+            $table->string('currency');
+            $table->float('amount',8,2);
+            $table->unsignedBigInteger('order_number')->nullable();
+            $table->unsignedBigInteger('invoice_no');
+            $table->string('order_date');
+            $table->string('order_month');
+            $table->string('order_year');
+            $table->string('confirmed_date')->nullable();
+            $table->string('processing_date')->nullable();
+            $table->string('picked_date')->nullable();
+            $table->string('shipped_date')->nullable();
+            $table->string('delivered_date')->nullable();
+            $table->string('cancel_date')->nullable();
+            $table->string('return_date')->nullable();
+            $table->string('return_reason')->nullable();
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
