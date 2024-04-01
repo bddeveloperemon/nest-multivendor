ebd86adcf7bb26a1e9ad7d40a38513b1c7920418
unknown <Emon>
2024-04-02 01:07:04 +0600

    Admin Panel Read All Reigster Vendor

--- app/Http/Controllers/Backend/Admin/ActiveUserController.php
+++ app/Http/Controllers/Backend/Admin/ActiveUserController.php
@@ -8,9 +8,17 @@ use App\Http\Controllers\Controller;
 
 class ActiveUserController extends Controller
 {
+   // Read all register users
     public function allUser()
     {
         $users = User::where('role','user')->latest()->get();
         return view('backend.user.user_data', compact('users'));
+    }
+
+   // Read all register vendors
+    public function allVendor()
+    {
+        $vendors = User::where('role','vendor')->latest()->get();
+        return view('backend.user.vendor_data', compact('vendors'));
     }
 }
--- resources/views/backend/user/vendor_data.blade.php
+++ resources/views/backend/user/vendor_data.blade.php
@@ -1,17 +1,17 @@
 @extends('backend.admin.dashboard')
 @section('admin_title')
-    Admin - All User Data
+    Admin - All Vendor Data
 @endsection
 @section('admin_content')
     <!--breadcrumb-->
     <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-        <div class="breadcrumb-title pe-3">All User Data</div>
+        <div class="breadcrumb-title pe-3">All Vendor Data</div>
         <div class="ps-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb mb-0 p-0">
                     <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
                     </li>
-                    <li class="breadcrumb-item active" aria-current="page">All User Data</li>
+                    <li class="breadcrumb-item active" aria-current="page">All Vendor Data</li>
                 </ol>
             </nav>
         </div>
@@ -41,27 +41,27 @@
                                     @php
                                         $i = 1;
                                     @endphp
-                                    @foreach ($users as $user)
+                                    @foreach ($vendors as $vendor)
                                         <tr role="row" class="odd">
                                             <td class="sorting_1">{{ $i++ }}</td>
                                             <td>
-                                                @if (!empty($user->image))
-                                                    <img src="{{ asset('upload/user_images/' . $user->image) }}"
-                                                        alt="{{ $user->name }}" class="rounded p-1" width="60px"
+                                                @if (!empty($vendor->image))
+                                                    <img src="{{ asset('upload/vendor_images/' . $vendor->image) }}"
+                                                        alt="{{ $vendor->name }}" class="rounded p-1" width="60px"
                                                         height="60px">
                                                 @else
                                                     <img src="{{ asset('backend/assets/images/avatars/avatar-500.png') }}"
-                                                        alt="User" class="rounded-circle p-1" width="100">
+                                                        alt="Vendor" class="rounded-circle p-1" width="100">
                                                 @endif
                                             </td>
-                                            <td>{{ $user->name }}</td>
-                                            <td>{{ $user->email }}</td>
-                                            <td>{{ $user->phone }}</td>
+                                            <td>{{ $vendor->name }}</td>
+                                            <td>{{ $vendor->email }}</td>
+                                            <td>{{ $vendor->phone }}</td>
                                             <td>
-                                                <span class="badge badge-pill bg-success">{{ $user->status }}</span>
+                                                <span class="badge badge-pill bg-success">{{ $vendor->status }}</span>
                                             </td>
                                             <td>
-                                                <a href="{{ route('admin.delete.subcategory', $user->id) }}"
+                                                <a href="{{ route('admin.delete.subcategory', $vendor->id) }}"
                                                     class="btn btn-danger btn-sm" id="delete">Delete</a>
                                             </td>
                                         </tr>
